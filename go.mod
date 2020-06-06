bacdd8e106ea408b65c7b061f3c3b80de46428a2
belsakn <belsak@gmail.com>
2020-06-06 09:22:51 +0200

    feat(): test module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/belsakn/testmodule
+
+go 1.14
---
+++ testmodule.go
@@ -0,0 +1,9 @@
+package testmodule
+
+func SayHello() string {
+	return "Try to say: Hello1."
+}
+
+func SayUUID() string {
+	return "12312-123123sdfsdfsd-cs32f-sd23rvds-1"
+}
